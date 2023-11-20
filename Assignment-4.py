6572da6651aedf0e4203b9ac3c289e68ce189852
Rafik Hijazy <rafikhz90@gmail.com>
2023-11-21 01:13:08 +0200

    Commit 2

--- Assignment-4.py
+++ Assignment-4.py
@@ -35,11 +35,34 @@ class LinkedList:
         while current!=None:
             print(current.Data)
             current=current.next
-link=LinkedList()
+"""link=LinkedList()
 link.add(1)
 link.add(2)
 link.add(3)
 link.add(4)
 link.displayList()
 link.delete(4)
-link.displayList()
+link.displayList()"""
+class Queue:
+    def __init__(self):
+        self.queue=[]
+    def isEmpty(self):
+        return len(self.queue)==0
+    def enqueue(self,val):
+        self.queue.append(val)
+    def dequeue(self):
+        if not self.isEmpty():
+         return self.queue.pop(0)
+        return
+    def peekFirst(self):
+        return self.queue[0]
+    def peekLast(self):
+        return self.queue[len(self.queue)-1]
+queue=Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+while not queue.isEmpty():
+    print(queue.dequeue())
+print(queue.isEmpty())
